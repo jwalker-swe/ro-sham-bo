a143b5192be8812de56444feb9420d8aa9ec8956
PRISM-EXE <jwalker.swe@gmail.com>
2024-12-31 15:37:31 -0600

    began working on play again functionality

--- src/components/scripts/index.js
+++ src/components/scripts/index.js
@@ -1,13 +1,15 @@
 const heading = document.querySelector("#heading");
 const iconContainer = document.querySelector("#rps-icons");
 const playButton = document.querySelector("#play-button");
+const playAgainButton = document.querySelector("#play-again-button");
 
 const iconRock = document.querySelector("#iconRock");
 const iconPaper = document.querySelector("#iconPaper");
 const iconScissors = document.querySelector("#iconScissors");
 
+let gameState = "start";
 
-
+// Create function to determine computer choice
 const cc = function() {
     let odds = Math.random();
 
@@ -20,6 +22,7 @@ const cc = function() {
     }
 }
 
+// Create function to determine winner: player or computer
 const dw = function(arg1, arg2) {
     rules = {
         rock: {rock: 'draw', paper: 'you lose', scissors: 'you win'},
@@ -31,50 +34,87 @@ const dw = function(arg1, arg2) {
     return results;
 }
 
+// Create function to add in a play again button after a game is played
+const addPlayAgainButton = function() {
+    const playAgainButton = document.createElement("button");
+
+    iconContainer.insertAdjacentElement("afterend", playAgainButton);
+
+    const playAgainContent = document.createTextNode("PLAY AGAIN");
+    playAgainButton.appendChild(playAgainContent);
+    playAgainButton.classList.add('button');
+    playAgainButton.id = "play-again-button";
+}
+
+// Create funciton to start game of rock paper scissors
 const startGame = function() {
-
-    //Declare variables used
-    let computerChoice = cc();
-
-    playButton.classList.add('hidden');
-    heading.innerText = 'MAKE YOUR SELECTION';
-
-    iconRock.classList.add('icon-button');
-    iconPaper.classList.add('icon-button');
-    iconScissors.classList.add('icon-button');
-
-    console.log(`Computer chose: ${computerChoice}`);
-
-    iconRock.addEventListener('click', () => {
-        iconRock.classList.remove('icon-button');
-        iconPaper.remove();
-        iconScissors.remove();
+    
+    // Determine current game state, either start or reset. Start represents a game state in which we are ready to begin. Reset represents a game state in which the board needs to be reset.
+    if (gameState === "start") {
         
-        playerChoice = 'rock';
-        console.log(`You chose: ${playerChoice}`);
+        // Declare variables used
+        let computerChoice = cc();
+        console.log(`Computer chose: ${computerChoice}`);
 
-        heading.innerText = `${dw(playerChoice, computerChoice).toUpperCase()}`
-    });
-    iconPaper.addEventListener('click', () => {
-        iconPaper.classList.remove('icon-button');
-        iconRock.remove();
-        iconScissors.remove();
+        // Update UI
+        playButton.remove();
+        heading.innerText = 'MAKE YOUR SELECTION';
 
-        playerChoice = 'paper';
-        console.log(`You chose: ${playerChoice}`);
+        iconRock.classList.add('icon-button');
+        iconPaper.classList.add('icon-button');
+        iconScissors.classList.add('icon-button');
 
-        heading.innerText = `${dw(playerChoice, computerChoice).toUpperCase()}`
-    });
-    iconScissors.addEventListener('click', () => {
-        iconScissors.classList.remove('icon-button');
-        iconRock.remove();
-        iconPaper.remove();
+        // Determine if the player has chosen rock, paper, or scissors based on which icon is clicked
+        iconRock.addEventListener('click', () => {
+            // Update UI
+            iconRock.classList.remove('icon-button');
+            iconPaper.remove();
+            iconScissors.remove();
+            
+            // Set playerChoice variable
+            playerChoice = 'rock';
+            console.log(`You chose: ${playerChoice}`);
 
-        playerChoice = 'scissors';
-        console.log(`You chose: ${playerChoice}`);
+            // Update heading text
+            heading.innerText = `${dw(playerChoice, computerChoice).toUpperCase()}`
 
-        heading.innerText = `${dw(playerChoice, computerChoice).toUpperCase()}`
-    });
+            // Add button to play again
+            addPlayAgainButton();
+        });
+        iconPaper.addEventListener('click', () => {
+            // Update UI
+            iconPaper.classList.remove('icon-button');
+            iconRock.remove();
+            iconScissors.remove();
+
+            // Set playerChoice variable
+            playerChoice = 'paper';
+            console.log(`You chose: ${playerChoice}`);
+
+            // Update heading text
+            heading.innerText = `${dw(playerChoice, computerChoice).toUpperCase()}`
+
+            // Add button to play again
+            addPlayAgainButton();
+        });
+        iconScissors.addEventListener('click', () => {
+            // Update UI
+            iconScissors.classList.remove('icon-button');
+            iconRock.remove();
+            iconPaper.remove();
+
+            // Set playerChoice variable
+            playerChoice = 'scissors';
+            console.log(`You chose: ${playerChoice}`);
+
+            // Update heading text
+            heading.innerText = `${dw(playerChoice, computerChoice).toUpperCase()}`
+
+            // Add button to play again
+            addPlayAgainButton();
+        });
+    }
 }
 
 playButton.addEventListener('click', startGame); 
+playAgainButton.addEventListener('click', playAgain);
